96186e33e21e98c42ded97d0f843b2e481c97310
Polar Humenn <polar@adiron.com>
2013-03-26 13:45:55 -0400

    update

--- app/controllers/centro_routes_controller.rb
+++ app/controllers/centro_routes_controller.rb
@@ -11,7 +11,11 @@ class CentroRoutesController < ApplicationController
     routes = CentroRoute.retrieve(params[:id])
     route = routes.select {|x| x.pattern_code == params[:pattern]}
     if route && route.count == 1
-      send_data route[0].to_kml, :filename => "Route #{route[0].route_code} Pattern #{route[0].pattern_code}.kml", :content_type => "application/xml"
+      if params[:with_stops]
+        send_data route[0].to_journey_kml, :filename => "Route #{route[0].route_code} Pattern #{route[0].pattern_code}.kml", :content_type => "application/xml"
+      else
+        send_data route[0].to_kml, :filename => "Route #{route[0].route_code} Pattern #{route[0].pattern_code}.kml", :content_type => "application/xml"
+      end
     else
       send_data "No KML", :content_type => "application/text"
     end
--- app/models/centro_route.rb
+++ app/models/centro_route.rb
@@ -4,6 +4,66 @@ class CentroRoute
   attr_accessor :coordinates
   attr_accessor :pattern_code
   attr_accessor :route_code
+  attr_accessor :stops
+  attr_accessor :links
+
+  class Stop
+    attr_accessor :point
+    attr_accessor :name
+    attr_accessor :id
+
+    def initialize
+
+    end
+
+    def to_kml(i = nil)
+      name = self.name.gsub("<", "&lt;").gsub("&", "&amp;").gsub(">", "&gt;")
+      if i
+        kml = "<Placemark id='sp_#{i}'><name>sp_#{i}:#{name}</name>"
+      else
+        kml = "<Placemark><name>#{name}</name>"
+      end
+
+      kml += "<Point><coordinates>"
+      kml += "#{point[0]}"
+      kml += ","
+      kml += "#{point[1]}"
+      kml += "</coordinates></Point>"
+
+      kml += "</Placemark>"
+      kml
+    end
+  end
+
+  class Link
+    attr_accessor :coordinates
+    attr_accessor :to, :from
+
+    def initialize
+      @coordinates = []
+    end
+
+    def to_journey_kml(i = nil)
+      html = ""
+      if i
+        html += "<Placemark id='link_#{i}'><name>link_#{i}</name>"
+      else
+        html += "<Placemark>"
+      end
+      data = coordinates.map { |lon, lat| "#{lon},#{lat}" }.join(" ")
+      html += "<LineString><coordinates>"
+      html += data
+      html += "</coordinates></LineString>"
+      html += "</Placemark>"
+      html
+    end
+  end
+
+  def initialize
+    @stops = []
+    @links = []
+  end
+
 
   def self.url(route_code)
     "http://bus-time.centro.org/bustime/map/getRoutePoints.jsp?route=#{route_code}&key=0.9038479823691"
@@ -20,6 +80,23 @@ class CentroRoute
             h = CentroRoute.new
             h.route_code=route["route"]["id"]
             h.pattern_code = r["id"]
+            link = Link.new
+            r["pt"].each do |x|
+              link.coordinates << [x["lon"], x["lat"]]
+              if x["bs"]
+                stop = Stop.new
+                stop.id = x["bs"]["id"]
+                stop.name = x["bs"]["st"]
+                stop.point = [x["lon"],x["lat"]]
+                link.to = stop
+                h.stops << stop
+                h.links << link
+                link = Link.new
+                link.from = stop
+              end
+            end
+            # remove the first and last links
+            h.links = h.links.take(h.links.length-1).drop(1)
             h.coordinates = r["pt"].map {|x| [x["lon"], x["lat"]] }
             res << h
           elsif r["pt"].is_a? Hash
@@ -28,6 +105,29 @@ class CentroRoute
       end
     end
     return res
+  end
+
+  def to_journey_kml
+    html = ""
+    html += "<kml xmlns='http://earth.google.com/kml/2.0'>"
+    html += "<Document><Folder><name>Route #{route_code} Pattern #{pattern_code}</name>"
+
+    sp_n = 0
+    link_n = 0
+    if links.nil? || links.length < 1
+      raise "No Links"
+    end
+    html += links.first.from.to_kml(sp_n)
+    links.each do |jptl|
+      html += jptl.to_journey_kml(link_n)
+      link_n += 1
+      sp_n += 1
+      html += jptl.to.to_kml(sp_n)
+    end
+
+    html += "</Folder></Document>"
+    html += "</kml>"
+    html
   end
 
   def to_kml
