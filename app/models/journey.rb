6da3d1034dfa5e7aca046c5b35e1961edf9dcedf
Polar Humenn <polar@adiron.com>
2013-10-03 15:29:18 -0400

    On the way to examining Busme! api.

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -24,7 +24,7 @@ class Journey < ActiveRecord::Base
   def p_dist(time_now)
     res =  (average_speed * (time_now - start_time)/60.0) / path_distance * 100
     res = [0,res].min
-    res = [res, 100].max
+    res = [res, 100].min
   end
 
   def start_time
